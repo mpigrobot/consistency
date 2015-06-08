de18d1303d2fcb353c02e381b8c0ddc516b80ff9
Liying0824 <85680757@qq.com>
2015-06-08 22:29:37 +0800

    change list

--- consistency/Nonlinear Estimator/+nonlinear/motionModel.m
+++
@@ -1,4 +0,0 @@
-function Xt1 = motionModel(Xt, Ut)
-    Xt1 = compound(Xt, Ut);
-end
-
--- consistency/Nonlinear Estimator/Utilities/predictEKF.m
+++
@@ -1,4 +0,0 @@
-function [muPred, SigmaPred] = predictEKF(mu, Sigma, u, motionModel, F, Q)
-    muPred    = motionModel(mu, u);
-    SigmaPred = F * Sigma * F' + Q;
-end
